89656abd382e9b0d5a8cd4f0f52967fe8979413f
codicate <codicate@gmail.com>
2021-04-12 14:41:22 -0400

    exportModule option for encompile

--- emcompile/index.js
+++ emcompile/index.js
@@ -1,10 +1,14 @@
 import { execSync } from 'child_process';
 
 const emscriptenHelper = (args) => {
-  const compileFunctions = args.slice(3)
+  const compileOptions = (args[2] && args !== '.') ? `-${args[2]}` : '';
+
+  const exportModule = (args[3] === true) ? '-s EXPORT_ES6=1 -s MODULARIZE=1' : '';
+
+  const compileFunctions = args.slice(4)
     .reduce((str, funcName) => str += `, '_${funcName}'`, '');
 
-  const cmd = `em++ ${args[0]} -o ${args[1]} -${args[2]} -s WASM=1 -s  EXTRA_EXPORTED_RUNTIME_METHODS=['ccall','cwrap'] -s EXPORTED_FUNCTIONS="['_main'${compileFunctions}]"`;
+  const cmd = `em++ ${args[0]} -o ${args[1]} ${compileOptions} ${exportModule} -s WASM=1 -s EXTRA_EXPORTED_RUNTIME_METHODS=['ccall','cwrap'] -s EXPORTED_FUNCTIONS="['_main'${compileFunctions}]"`;
 
   execSync(cmd, { encoding: 'utf-8' });
 };
@@ -17,5 +21,3 @@ const emscriptenHelper = (args) => {
     console.log(err);
   }
 })();
-
-// 'EXPORT_ES6=1 -s MODULARIZE=1 -s'

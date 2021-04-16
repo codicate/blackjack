5112fed81064da72853e7bfcc6dc00d74ccc9f3e
codicate <codicate@gmail.com>
2021-04-16 00:14:25 -0400

    emcompile refactor

--- emcompile/index.js
+++ emcompile/index.js
@@ -1,23 +1,29 @@
 import { execSync } from 'child_process';
 
 const emcompile = (args) => {
-  const compileOptions = (args[2] && args !== '.') ? `-${args[2]}` : '';
+  const compileOptions = (args[2] && args[2] !== '.')
+    ? `-${args[2]}`
+    : '';
 
-  const exportModule = (args[3] === true) ? '-s EXPORT_ES6=1 -s MODULARIZE=1' : '';
+  const exportModule = (args[3] === 'export')
+    ? '-s EXPORT_ES6=1 -s MODULARIZE=1'
+    : '';
 
   const compileFunctions = args.slice(4)
-    .reduce((str, funcName) => str += `, '_${funcName}'`, '');
+    .reduce((str, funcName) =>
+      str += `, '_${funcName}'`
+      , ''
+    );
 
   const cmd = `em++ ${args[0]} -o ${args[1]} ${compileOptions} ${exportModule} -s WASM=1 -s EXTRA_EXPORTED_RUNTIME_METHODS=['ccall','cwrap'] -s EXPORTED_FUNCTIONS="['_main'${compileFunctions}]"`;
 
   execSync(cmd, { encoding: 'utf-8' });
 };
 
-(() => {
-  try {
-    const args = process.argv.slice(2);
-    emcompile(args);
-  } catch (err) {
-    console.log(err);
-  }
-})();
+try {
+  const args = process.argv.slice(2);
+  emcompile(args);
+
+} catch (err) {
+  console.err(err);
+}
